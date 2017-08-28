90683c407a41c2f1e5bd321bda2efee9513158ca
Alexande Zolotov <slayer666@debian-slayer666.alexander.zolotov.net>
2017-08-28 23:30:16 +0300

    class App - changed output title text;class SideMenu - added menu items output as <li><button></button></li>, added dummy handleClick() method;added sideMenu.css for styling SideMenu's presentation

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-      <h1>Hello, world.</h1>
+      <h1>Free Advertisements.</h1>
       <SideMenu/>
       <AccountSettings/>
       <SearchHeader/>
--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import "../css/sideMenu.css";
 
 /*Drop-right menu on the left side of the application.
   Contains links to: Latest Ads, Create Advertisement, Saved Advertisements e.t.c.
@@ -11,11 +12,25 @@ class SideMenu extends React.Component {
     this.state = {className: "SideMenu"};
   }
 
+  handleClick(e) {
+    console.log('this is:', e.target['id']);
+  }
+
   render() {
+    let itemNames = ["All Ads", "Saved Ads", "My Ads", "New Add", "Account Settings", "Log out"];
+    let itemIds = ["mu_item_all_ads", "mu_item_saved_ads", "mu_item_my_ads", "mu_item_anew_ad", "mu_item_acc_settings", "mu_item_log_out"];
+
+    let menuItems = [];
+
+    for (let i = 0; i < itemNames.length; ++i) {
+      menuItems.push(<li key={i} className="side_menu_nav_list_item"><button id={itemIds[i]} onClick={(e) => this.handleClick(e)}>{itemNames[i]}</button></li>);
+    }
+
     return (
-      <div>
-       <div><h3>Hello, I am: "{this.state.className}"</h3></div>
-       <div><em>Implement me!</em></div>
+      <div id ="slider_menu">
+        <ul className="side_menu_nav_list">
+          {menuItems}
+        </ul>
       </div>
     );
   }
